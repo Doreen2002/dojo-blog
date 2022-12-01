b213ebd9120494217f2798a29c0e211cf3b5c109
Doreen2002 <doreenmwapekatebe8@gmail.com>
2022-12-01 17:19:11 +0200

    tutorial no 8

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,8 @@
+import { useState } from "react";
 const Home = ()=>
 {
+  const [name, setName] = useState('mario'); 
+  const [age, setAge] = useState(25); 
     const handleClick = (e) =>
     {
         console.log('hello', 'ninjas', e.target);
@@ -8,6 +11,8 @@ const Home = ()=>
     const handleClickAgain = (name)=>
     {
         console.log('helle' + name)
+        setName('Mwape')
+        setAge(20)
     }
 
   return(
@@ -15,6 +20,7 @@ const Home = ()=>
     <>
     <div className="home">
         <h2>Homepage</h2>
+        <p>{name} is {age}</p>
         <button onClick={handleClick}>Click Me</button>
         <button onClick={()=>handleClickAgain('doreen')}>Click Me Again</button>
     </div>
