95ccce63bfc06db30f8c7fbb6038b39de79d7fe7
Doreen2002 <doreenmwapekatebe8@gmail.com>
2022-12-01 17:10:57 +0200

    tutorial no 7

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
 const Home = ()=>
 {
+    const handleClick = (e) =>
+    {
+        console.log('hello', 'ninjas', e.target);
+    }
+
+    const handleClickAgain = (name)=>
+    {
+        console.log('helle' + name)
+    }
+
   return(
+    
     <>
     <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={()=>handleClickAgain('doreen')}>Click Me Again</button>
     </div>
     </>
   );
