0af4a0263fccfa5d3403c65de647921a17d7db0a
Doreen2002 <doreenmwapekatebe8@gmail.com>
2022-12-02 16:21:23 +0200

    tutorial 10

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,23 @@
 import { useState } from "react";
 const Home = ()=>
 {
-  const [name, setName] = useState('mario'); 
-  const [age, setAge] = useState(25); 
-    const handleClick = (e) =>
-    {
-        console.log('hello', 'ninjas', e.target);
-    }
-
-    const handleClickAgain = (name)=>
-    {
-        console.log('helle' + name)
-        setName('Mwape')
-        setAge(20)
-    }
+  const [blogs, setBlogs] = useState([
+    {title: 'My new website', body:'lorem dummy data',author:'gideon', id:1},
+    {title: 'My new website2', body:'lorem dummy data', author:'alita',id:2},
+    {title: 'My new website3', body:'lorem dummy data',author:'doreen', id:3}
+  ])
 
   return(
     
     <>
     <div className="home">
         <h2>Homepage</h2>
-        <p>{name} is {age}</p>
-        <button onClick={handleClick}>Click Me</button>
-        <button onClick={()=>handleClickAgain('doreen')}>Click Me Again</button>
+        <div>
+        {blogs.map((item)=>
+        <div key={item.id}><h1>{item.title} written by {item.author}</h1><span>{item.body}</span></div>
+          
+        )}
+        </div>
     </div>
     </>
   );
